027680aedc679dad9b24d432703861ee6b463349
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-08-11 21:45:28 -0300

    fixed norm and leaks so the main branch would be clean

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ghenaut- <ghenaut-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:24:39 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/11 21:15:47 by ghenaut-         ###   ########.fr       */
+/*   Updated: 2022/08/11 21:40:04 by ghenaut-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-int print_and_return(void);
 int	init_shell(void);
 
 #endif
--- srcs/init_shell.c
+++ srcs/init_shell.c
@@ -6,7 +6,7 @@
 /*   By: ghenaut- <ghenaut-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 21:11:31 by ghenaut-          #+#    #+#             */
-/*   Updated: 2022/08/11 21:39:14 by ghenaut-         ###   ########.fr       */
+/*   Updated: 2022/08/11 21:43:04 by ghenaut-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ int	init_shell(void)
 {
 	char	*line;
 
-	while(1)
+	while (1)
 	{
 		line = readline("minishell> ");
 		if (line && *line)
 			add_history(line);
+		if (ft_strncmp(line, "exit", 5) == 0)
+			break;
 	}
+	rl_clear_history();
+	free(line);
 	return (0);
 }
