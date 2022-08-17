9f8546b42a2a6be984f224791856a45b315e7e3d
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-08-16 23:02:12 -0300

    changed the struct to have everything

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ghosthologram <ghosthologram@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:24:39 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/13 19:19:47 by ghosthologr      ###   ########.fr       */
+/*   Updated: 2022/08/16 23:01:04 by ghosthologr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,18 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+// tirei o err file, pq aparentemente a gente nao tem que fazer a parte de mudar o stderr, e pra usar sempre o padrao
+
 typedef struct s_cmd_table
 {
-	t_list	*table; //vai ser uma lista de listas, pois cada lista vai ter uma lista do comando com as suas flags e tals
-	int		size;
+	char 	***table;	// array com os comandos
+	char	*infile;	// por padrao STDIN_FILENO, ou o nome do arquive se a pessoa mudar
+	char	*outfile;	// por padrao STDOUT_FILENO, ou o nome do arquive se a pessoa mudar
+	int		n_of_pipes; // numero de pipes
+	int		n_of_cmds;	// numeros de comandos
+	int		here_doc;	// 0 por padrao, 1 se a pessoa tiver usando here_doc
+	char	*limiter;	// limeter para sair do here_doc
+	int		append;		// 0 por padrao, 1 se a pessoa tiver especificado uma outfile e especificado append inves de sobreescrever
 }				t_cmd_table;
 
 extern t_cmd_table *cmd_table;
