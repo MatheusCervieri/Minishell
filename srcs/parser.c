22a70e0ad83935b25f7c780b7fae736c0c2b7976
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-08-19 20:31:53 -0300

    just syncing machines

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: ghosthologram <ghosthologram@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 22:07:41 by ghenaut-          #+#    #+#             */
-/*   Updated: 2022/08/15 13:55:44 by ghosthologr      ###   ########.fr       */
+/*   Updated: 2022/08/19 16:36:18 by ghosthologr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,79 @@ t_list	*lexer(char *line)
 	return (rtn);
 }
 
+void init_global(void)
+{
+	cmd_table->infile = 0;
+	cmd_table->outfile = 0;
+	cmd_table->here_doc = 0;
+	cmd_table->n_of_cmds = 0;
+	cmd_table->n_of_pipes = 0;
+	cmd_table->append = 0;
+}
+
+int	is_special(char *arg)
+{
+	return (*arg == '<' || *arg == '>' || *arg == '|');
+}
+
+void	handle_special(t_list *lst)
+{
+	if(ft_strncmp((char *)lst->content, "|", 2) == 0)
+		cmd_table->n_of_pipes++;
+	else
+	{
+		if(ft_strncmp((char *)lst->content, "<<", 2) == 0)
+		{
+			cmd_table->here_doc = 1;
+			cmd_table->limiter = ft_strdup((char *)lst->next->content);
+		}
+		else if(ft_strncmp((char *)lst->content, ">>", 2) == 0)
+		{
+			cmd_table->append = 0;
+			cmd_table->outfile = ft_strdup((char *)lst->next->content);
+		}
+		else if(ft_strncmp((char *)lst->content, "<", 2) == 0)
+			cmd_table->infile = ft_strdup((char *)lst->next->content);
+		else if(ft_strncmp((char *)lst->content, ">", 2) == 0)
+			cmd_table->outfile = ft_strdup((char *)lst->next->content);
+		lst = lst->next;
+	}
+}
+
+int	parser(t_list *tmp)
+{
+	t_list	*ptr;
+	t_list	*tmp;
+	int		new_cmd;
+
+	ptr = tmp;
+	new_cmd = 0;
+	while(ptr)
+	{
+		if (is_special((char*)ptr->content))
+			handle_special(ptr);
+		else
+		{
+			tmp = 
+			while (ptr && ft_strncmp((char *)ptr->content, "|", 1))
+			{
+
+			}
+		}
+	}
+}
+
 int	parse_line(char *line)
 {
-	t_list *tmp;
+	t_list	*tmp;
+
 	if (!line)
 		return (0);
 	if (ft_strncmp(line, "exit", 5) == 0)
 		return (1);
+	init_global();
 	tmp = lexer(line);
-	//TODO BUILD PARSER
-	if (tmp)
-		return(2);
+	if (!parser(tmp))
+		//handle error
 	return (0);
 }
