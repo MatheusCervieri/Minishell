974f65d8f609e91d1ac1f2a17a8ece46b20b6482
MatheusCervieri <mathcervieri@gmail.com>
2022-08-22 17:15:44 -0300

    I created print_struct function that prints our global struct - i will use this to test some functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,56 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 20:23:53 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/22 16:45:44 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/22 17:14:58 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void print_struct(void)
+{
+	int i;
+	i = 0;
+	printf("**table\n");
+	while (g_cmd_table->table[i])
+	{
+		printf("COMAND TABLE:::");
+		printf("%s\n\n", g_cmd_table->table[i]);
+		i++;
+	}
+	printf("\n");
+	printf("n_of_cmds\n");
+	printf("%i", g_cmd_table->n_of_cmds);
+	printf("\n");
+	printf("infile\n");
+	printf("%s", g_cmd_table->infile);
+	printf("\n");
+	printf("outfile\n");
+	printf("%s", g_cmd_table->outfile);
+	printf("\n");
+	printf("infile_exists\n");
+	printf("%i", g_cmd_table->infile_exists);
+	printf("\n");
+	printf("outfile_exists\n");
+	printf("%i", g_cmd_table->outfile_exists);
+	printf("\n");
+	printf("here_doc\n");
+	printf("%i", g_cmd_table->here_doc);
+	printf("\n");
+	printf("append\n");
+	printf("%i", g_cmd_table->append);
+	printf("\n");
+	printf("limiter\n");
+	printf("%s", g_cmd_table->limiter);
+	printf("\n");
+	printf("status\n");
+	printf("%i", g_cmd_table->status);
+	printf("\n");
+	printf("last_status\n");
+	printf("%i", g_cmd_table->last_status);
+	printf("\n");
+	
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -51,26 +96,18 @@ int	main(int argc, char **argv, char **envp)
 	g_cmd_table->status = 0;
 	
 	
-	line = ft_strdup("grep a | grep a | grep d");
+	line = ft_strdup("< text1.txt grep a | grep a | grep d > text2.txt");
 	init_global();
 	rtn = 0;
 	rtn = parse_line(line);
 	g_cmd_table->infile_exists = 1;
 	g_cmd_table->outfile_exists = 1;
 	g_cmd_table->infile = ft_strdup("text1.txt");
-	g_cmd_table->outfile = ft_strdup("text14.txt");
+	printf("OUTFILE: %s\n", g_cmd_table->outfile);
 	int i;
 	i = 0;
 	g_cmd_table->n_of_cmds = g_cmd_table->n_of_cmds - 1;
-	printf("NCMDS%i \n\n",g_cmd_table->n_of_cmds);
-	while (g_cmd_table->table[i])
-	{
-		printf("COMAND TABLE:::");
-		printf("%s\n\n", g_cmd_table->table[i]);
-		i++;
-	}
+	print_struct();
 	executor_handler(envp);
 	return (rtn);
-	
-
 }
