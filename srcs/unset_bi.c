4ae9a7a4e8e0cf416057800d838b4e115c312143
MatheusCervieri <mathcervieri@gmail.com>
2022-08-30 17:05:41 -0300

    More norm fixing

--- srcs/unset_bi.c
+++ srcs/unset_bi.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:18:03 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/30 16:31:46 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/30 17:05:11 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	lst_find_var_unset(t_list *head, char *var_name)
 {
-	int p;
-	char *tmp;
+	int		p;
+	char	*tmp;
 
 	p = 0;
 	while (head)
@@ -30,34 +30,47 @@ int	lst_find_var_unset(t_list *head, char *var_name)
 	return (-1);
 }
 
-void	remove_node(t_list **head, int position)
+void	remove_node_loop(t_list	**aux_aux, t_list **prev_node_aux,
+		t_list **next_node_aux, int position)
 {
 	t_list	*aux;
 	t_list	*prev_node;
 	t_list	*next_node;
 	int		i;
 
+	aux = *aux_aux;
+	prev_node = *prev_node_aux;
+	next_node = *next_node_aux;
+	i = 0;
+	while (aux)
+	{
+		next_node = aux->next;
+		if (i == position)
+		{
+			prev_node->next = next_node;
+			free(aux);
+			break ;
+		}
+		prev_node = aux;
+		i++;
+		aux = aux->next;
+	}
+}
+
+void	remove_node(t_list **head, int position)
+{
+	t_list	*aux;
+	t_list	*prev_node;
+	t_list	*next_node;
+
 	if (position >= 0)
 	{
 		aux = *head;
-		i = 0;
 		if (position == 0)
 			*head = (*head)->next;
 		else
 		{
-			while (aux)
-			{
-				next_node = aux->next;
-				if (i == position)
-				{
-					prev_node->next = next_node;
-					free(aux);
-					break ;
-				}
-				prev_node = aux;
-				i++;
-				aux = aux->next;
-			}
+			remove_node_loop(&aux, &prev_node, &next_node, position);
 		}
 	}
 }
