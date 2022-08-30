efeac626bf40d4b70c97cc3267230dc2dde13f05
MatheusCervieri <mathcervieri@gmail.com>
2022-08-30 16:33:57 -0300

    More norm fixing

--- srcs/unset_bi.c
+++ srcs/unset_bi.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:18:03 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/30 13:19:23 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/30 16:31:46 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,47 +32,48 @@ int	lst_find_var_unset(t_list *head, char *var_name)
 
 void	remove_node(t_list **head, int position)
 {
+	t_list	*aux;
+	t_list	*prev_node;
+	t_list	*next_node;
+	int		i;
+
 	if (position >= 0)
 	{
-		t_list	*aux;
-		t_list	*prev_node;
-		t_list	*next_node;
-		int	i;
-
 		aux = *head;
 		i = 0;
-		if(position == 0)
+		if (position == 0)
 			*head = (*head)->next;
-	else
-	{
-		while (aux)
+		else
 		{
-			next_node = aux->next;
-			if (i == position)
+			while (aux)
 			{
-				prev_node->next = next_node;
-				free(aux);
-				break ;	
-			}
-			prev_node = aux;
-			i++;
-			aux = aux->next;
+				next_node = aux->next;
+				if (i == position)
+				{
+					prev_node->next = next_node;
+					free(aux);
+					break ;
+				}
+				prev_node = aux;
+				i++;
+				aux = aux->next;
 			}
 		}
-    }
+	}
 }
 
-void unset_bi(char **cmd_args)
+void	unset_bi(char **cmd_args)
 {
-    if (args_len(cmd_args) > 0)
-    {
-        int i;
+	int	i;
 
-        i = 1;
-        while (i < args_len(cmd_args))
-        {
-            remove_node(&(g_cmd_table->envp), lst_find_var_unset(g_cmd_table->envp, cmd_args[i]));
-            i++;
-        }
-    }
+	if (args_len(cmd_args) > 0)
+	{
+		i = 1;
+		while (i < args_len(cmd_args))
+		{
+			remove_node(&(g_cmd_table->envp),
+				lst_find_var_unset(g_cmd_table->envp, cmd_args[i]));
+			i++;
+		}
+	}
 }
