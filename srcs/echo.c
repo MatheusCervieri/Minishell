b0a361b5a2ef791f7ef915fcfece2c4dbc40039c
MatheusCervieri <mathcervieri@gmail.com>
2022-09-01 13:22:17 -0300

    I fix echo built in

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 11:00:07 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/30 12:50:46 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/09/01 13:21:40 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,23 @@
 
 void	write_echo(char **cmd_args, int parameter)
 {
-	int	i;
+	int		i;
+	char	*print_string;
 
 	i = parameter;
 	while (i < args_len(cmd_args))
 	{
-		ft_putstr_fd(cmd_args[i], 1);
+		if (cmd_args[i][0] == 34 || cmd_args[i][0] == 39)
+		{
+			if (cmd_args[i][0] == 34)
+				print_string = ft_strtrim(cmd_args[i], "\"");
+			if (cmd_args[i][0] == 39)
+				print_string = ft_strtrim(cmd_args[i], "\'");
+			ft_putstr_fd(print_string, 1);
+			free(print_string);
+		}
+		else
+			ft_putstr_fd(cmd_args[i], 1);
 		if (i != args_len(cmd_args) - 1)
 			ft_putstr_fd(" ", 1);
 		i++;
