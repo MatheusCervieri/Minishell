5e2fe3807c44ff1d100626ab968d3845965d3df8
MatheusCervieri <mathcervieri@gmail.com>
2022-09-01 11:10:16 -0300

    I FIX HERE_DOC SIGNAL UHULLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 13:25:16 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/09/01 10:58:27 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/09/01 11:07:08 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ void	ctrlc_handler(int sing)
 	g_cmd_table->status = 130;
 	if(g_cmd_table->here_doc == 1)
 		g_cmd_table->here_doc_execute = 0;
-	ft_putstr_fd("\n", 1);
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
+	else
+	{
+		ft_putstr_fd("\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
 void	ctrlc_child_handler(int sing)
@@ -38,5 +41,5 @@ void	ctrlc_here_doc_handler(int sing)
 	ft_putstr_fd("\n", 1);
 	rl_replace_line("", 0);
 	close(g_cmd_table->here_doc_file);
-	//exit(1);
+	exit(1);
 }
