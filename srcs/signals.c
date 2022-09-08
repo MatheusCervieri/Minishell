090bd87c15c44df77713cc1ab027c10179d96879
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2022-09-08 15:46:01 -0300

    fixed heredoc error

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: ghenaut- <ghenaut-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 13:25:16 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/09/05 19:25:26 by ghenaut-         ###   ########.fr       */
+/*   Updated: 2022/09/08 15:20:34 by ghenaut-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,6 @@ void	ctrlc_here_doc_handler(int sing)
 	ft_putstr_fd("\n", 1);
 	rl_replace_line("", 0);
 	close(g_cmd_table->here_doc_file);
-	free(g_cmd_table->limiter);
 	free_global();
 	clear_memory();
 	exit(1);
